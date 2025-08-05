a85cbffca96190f64753e75d6a9985001d8a1184
plesioo <phillipfleischer2@gmail.com>
2025-08-05 18:43:59 +0200

    feat: implement install command

--- subcommands/install.ts
+++ subcommands/install.ts
@@ -2,6 +2,22 @@ import { Command } from "@cliffy/command";
 
 const install = new Command()
   .description("Start tracking your git activities.")
-  .action(() => console.log("Gitely started tracking"));
+  .action(async () => {
+    const hookPath = ".git/hooks/post-commit";
+
+    const hookContent = `#!/bin/sh
+    # Gitely post-commit hook
+    deno run --allow-read --allow-write --allow-env /Users/phillipfleischer/Documents/gitely/main.ts track
+    `;
+
+    try {
+      await Deno.writeTextFile(hookPath, hookContent);
+      await Deno.chmod(hookPath, 0o755);
+    } catch (error) {
+      console.error("❌ Failed to install hook:", error);
+    }
+
+    console.log("✅ Gitely tracking successfully installed.");
+  });
 
 export default install;
